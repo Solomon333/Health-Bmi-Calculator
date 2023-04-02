a4598426e0f06774508c002024710ce157cbf2b1
Salomon Solomon <ssalomon783@gmail.com>
2023-04-02 18:25:37 +0300

    edit js and css files

--- app.js
+++ app.js
@@ -4,27 +4,30 @@ function bmiCalc() {
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
     let result = document.querySelector(".bmi-result");
-    let bmi =  (weight) / Math.pow(height/100, 2);
+    let bmi =  Math.round((weight) / Math.pow(height/100, 2).toFixed(2));
+    
     
     if(bmi < 18.5){
         result.style.color= "yellow";
 
-        result.innerHTML = "You Are UnderWeight " + bmi;
+        result.innerHTML = "Your BMI is: " + bmi + " UnderWeight";
     }
     if(bmi > 18.5 && bmi < 25.0){
         result.style.color= "green";
-        result.innerHTML = "You Are Healthy " + bmi;
+        result.innerHTML = "Your BMI is: " + bmi + " Healthy";
     }
     if(bmi > 25.0) {
         result.style.color= "red";
-        result.innerHTML = "You Are OverWeight " + bmi;
+        result.innerHTML = "Your BMI is: " + bmi +  " OverWeight";
     }
 
 
 }
 
   document.querySelector(".resetBtn").addEventListener("click", function() {
-     let clear = document.querySelector(".bmi-result");
-
-     clear.innerHTML = ''; 
+     document.querySelector(".bmi-result").innerHTML = '';
+      document.getElementById("weight").value = '';
+     document.getElementById("height").value = '';
   })
+
+//  
